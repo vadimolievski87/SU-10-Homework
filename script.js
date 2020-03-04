d383cc90460372de085b3b6f8dc14b1837c77e7b
Vadim Olievski <vadimolievsky@gmail.com>
2020-03-04 16:08:39 +0200

    SU-10-Homework

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
- // let myCurrentAge = 32;
-//if (myCurrentAge < 30) {
-    //document.write("I'am over 25 years old")
-//}else {
-    //document.write ("I'am less than 35 years")
-//}
+ /* let myCurrentAge = 32;
+if (myCurrentAge < 30) {
+      document.write("I'am over 25 years old")
+}else {
+      document.write ("I'am less than 35 years")
+} */
 
-let myCurrentAge = 32;
-myCurrentAge = 32 < 30 ? ("I'am over 25 years old") : ("I'am less than 35 years")
-document.write(myCurrentAge);
-// тернарный
+ 
+/* let myCurrentAge = 32;
+   myCurrentAge = 32 < 30 ? ("I'am over 25 years old") : ("I'am less than 35 years")
+   document.write(myCurrentAge);
+   тернарный */
+
+
+   let num = 33.5;
+console.log(typeof num);
+
+let str = "Undisputed";
+console.log(typeof str);
+
+let boo = true;
+console.log(typeof boo);
+
+console.log(typeof undif);
+
+/*let nul = null;
+console.group(typeof nul); */
+
+let obj = "Table"
+console.log(typeof obj)
