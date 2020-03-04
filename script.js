cd77a48acc05fc3705de6c712c014102c374a5b3
Vadim Olievski <vadimolievsky@gmail.com>
2020-03-04 15:07:43 +0200

    SU-10-Homework

---
+++ script.js
@@ -0,0 +1,11 @@
+ // let myCurrentAge = 32;
+//if (myCurrentAge < 30) {
+    //document.write("I'am over 25 years old")
+//}else {
+    //document.write ("I'am less than 35 years")
+//}
+
+let myCurrentAge = 32;
+myCurrentAge = 32 < 30 ? ("I'am over 25 years old") : ("I'am less than 35 years")
+document.write(myCurrentAge);
+// тернарный
